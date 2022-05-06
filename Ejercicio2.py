f90ca195dc384a2f369a826c37f92ee1e5a7cf10
sneyderuva <87355886+sneyderuva@users.noreply.github.com>
2022-05-06 09:12:24 -0500

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,27 @@
+from sqlalchemy import intersect
+
+
+def CDT(Usuario:str, Tiempo:int, Cantidad:int)
+
+    Usuario=(input("Ingrese su nombre: "))
+
+
+
+def Valor_Intereses():
+    Valor=float
+    Cantidad=float(input("Ingrese el valor: "))
+    Interes=float(input("Ingrese el valor del Interes en porcentaje: "))
+    Tiempo2=float(input("Ingrese el valor del tiempo: "))
+    Knste=12
+    
+    Valor=Cantidad*Interes*Tiempo2
+    Valor=Valor/Knste
+    print(Valor)
+    
+def Valor_Total(Cantidad,Interes):
+    Valor_interes=int
+    Valor_interes=Interes
+    Valor_interes= Valor_interes + Cantidad
+    print(Valor_interes)
+
+print(CDT())
